3dd229baab6dab363f7721e2a1163387f6b38730
RaimondoCosma <raimondo.cosma@gmail.com>
2022-11-11 14:32:32 +0100

    Aggiunge titolo e foto

---
+++ js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const { createApp } = Vue;
+createApp({
+data() {
+    return {
+    message: 'Sto utilizzando Vue!',
+    image: {
+            source: "https://cdn-media-1.freecodecamp.org/ghost/2019/03/vueart.png",
+            alt: "photo"
+        }
+    }
+}
+}).mount('#app')
