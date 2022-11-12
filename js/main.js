639e5d3834c279fd9d382994b637620f18591f32
RaimondoCosma <raimondo.cosma@gmail.com>
2022-11-12 21:44:57 +0100

    Aggiunge v-bind su alt

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ data() {
     return {
     message: 'Sto utilizzando Vue!',
     image: {
-            source: "https://cdn-media-1.freecodecamp.org/ghost/2019/03/vueart.png",
-            alt: "photo"
+        source: "https://cdn-media-1.freecodecamp.org/ghost/2019/03/vueart.png",
+        alt: "photo-vue"
         }
     }
 }
